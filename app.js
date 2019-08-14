4f4634d7b5b7d3068dfa7a205085e6482c420733
Satchel Page <satchelpage@SatchBookPro2.local>
2019-08-13 20:08:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,102 @@
+// Variables
+
+// var foo = 1; // integer
+// console.log(foo);
+// var baz = "Hello World"; // string 
+// console.log(baz);
+// var bar = 3 + 4; //What does bar contain?
+// console.log(bar);
+
+// var firstName = 'Herbert';
+
+// var lastName = 'Brown';
+
+// var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// var firstName = 'Cassandra';
+// console.log(firstName);
+
+// var lastName = 'Evans';
+// console.log(lastName);
+
+// lastName = 'Brown';
+// console.log(lastName);
+
+// var Chris;
+
+// Chris = 'Merry Christmas';
+// console.log(Chris);
+
+// var boo = 'fab';
+
+// var Boo = 'hey';
+
+// console.log(boo == Boo);
+
+// console.log(true || false); // true
+
+// console.log(false || true); // true
+
+// console.log(false && false); 
+
+// console.log(true && false);
+
+// console.log(2 == 2);
+
+// var roo = true;
+// console.log(roo);
+
+// var boo; 
+// boo = "I can't do this";
+// console.log(boo);
+
+// var add = 5 + 6;
+// var subtract = 6 - 3;
+// var mult = 6 * 4;
+// var div = 25 / 4;
+// subtract = 76- 43;
+
+// console.log(add);
+// console.log(mult);
+// console.log(div);
+// console.log(subtract);
+
+// var bigNumber = 45 * 394853;
+// console.log(bigNumber);
+
+// var foo = 1; 
+
+// var boo = "1";
+
+// var noo = "3"; 
+
+// var coo = foo + foo;
+
+// var roo = boo + noo;
+
+// var baz = foo + boo;
+
+// console.log("Coo will return" , coo);
+// console.log("Roo will return" , roo);
+// console.log("Baz will return" , baz);
+
+// Convert the current temperature from Fahrenheit into celsius
+
+//(fah - 32) * 5 / 9
+
+// console.log((89-32) * 5 / 9);
+
+var fahrenheit;
+fahrenheit = 212;
+fahrenheit = 90;
+
+var celsius = (fahrenheit - 32) * 5 / 9;
+console.log(celsius);
+console.log(celsius.toFixed(2));
+
+
+
+
+
+
---
+++ functions.js
@@ -0,0 +1,64 @@
+// functions yay!!!!
+
+function addFive(num) {
+    var result = num + 5;
+    console.log(result);
+}
+
+// addFive(10);
+
+function greeting() {
+    var hello = "hello world";
+    console.log(hello);
+}
+
+// greeting();
+
+function triangleArea(base, height) {
+    var area = 1/2 * base * height;
+    console.log(area);
+}
+
+// triangleArea(3, 4); // expects 6
+
+// triangleArea(5, );
+
+// var boo = 2;
+var foo = '2';
+
+// var baz = boo / foo;
+// console.log(baz);
+
+function boo(num) { // var num = 9;
+    var result = num * 12; // 9 * 12;
+    return result;
+}
+
+// boo(9); //expect 108
+
+// console.log(boo(9));
+
+function baz(foo) { // var foo = 9;
+    var newResult = boo(foo) / 2; // boo(9) -> 108 / 2;
+    console.log(newResult); // 54
+}
+
+// baz(9);
+
+function fahToCels(fah) {
+    var celsius = (fah - 32) * 5 / 9;
+    console.log(celsius);
+}
+
+fahToCels(212);
+fahToCels(89);
+fahToCels(32)
+
+// Write a function that will calculate the amount of PTO you receice in a year
+
+function ptoCalc(hours, checksPerYear) {
+    var ptoPerYear = hours * checksPerYear;
+    console.log(ptoPerYear); 
+}
+
+ptoCalc(4, 24);
